2ff624fd4aa119066d769fd7ce4f2bbd9264dc94
abdul muqeeth mohammed <abdulmuqeethmohammed@10-5-216-254.vlan876.client.wireless.uic.edu>
2018-02-09 15:47:40 -0600

    Implementing code for caputring number from TextView and regex matching

--- ActivitiesAndIntents/app/src/main/java/com/example/abdulmuqeethmohammed/activitiesandintents/InputNumberActivity.java
+++ ActivitiesAndIntents/app/src/main/java/com/example/abdulmuqeethmohammed/activitiesandintents/InputNumberActivity.java
@@ -1,14 +1,60 @@
 package com.example.abdulmuqeethmohammed.activitiesandintents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InputNumberActivity extends Activity {
+    private EditText numberField;
+    private String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_number);
+        numberField = (EditText) findViewById(R.id.phoneNumberWidget);
+        numberField.setOnEditorActionListener(numberListener);
+    }
+
+    public TextView.OnEditorActionListener numberListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                captureNumber();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    //Method to retrieve the number
+    public void captureNumber() {
+        Intent i = getIntent();
+        number = numberField.getText().toString();
+
+        if (number.matches("\\s*\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}\\s*")) {
+            Log.i("Regex Match", "True");
+            Toast.makeText(this, "Valid number", Toast.LENGTH_SHORT).show();
+            setResult(RESULT_OK, i);
+            i.putExtra("number", number);
+            //finish the activity
+            finish();
+        } else {
+            Log.i("Regex Match", "False");
+            setResult(RESULT_CANCELED, i);
+            i.putExtra("number", number);
+            //finish the activity
+            finish();
+        }
     }
 
 }
--- ActivitiesAndIntents/app/src/main/java/com/example/abdulmuqeethmohammed/activitiesandintents/MainActivity.java
+++ ActivitiesAndIntents/app/src/main/java/com/example/abdulmuqeethmohammed/activitiesandintents/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 
     private Button input;
     private Button dial;
+    private final int resultCode =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +53,7 @@ public class MainActivity extends Activity {
 
     // Method to start InputNumber Activity
     private void switchToInputNumber(){
-
+         Intent i = new Intent(MainActivity.this, InputNumberActivity.class);
+         startActivityForResult(i, resultCode);
     }
 }
